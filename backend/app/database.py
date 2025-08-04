b3bdf55101c44c62a7874e8ce84916f26f07c83c
Adrian Ruiz, Luis Abad, Diogo Sandoval <adrianalejandroruiz13@gmail.com>
2025-08-03 19:44:06 -0500

    agregando.

--- backend/app/database.py
+++ backend/app/database.py
@@ -11,3 +11,11 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+# ESTA FUNCIÓN ES NECESARIA PARA LOS DEPENDS DE FASTAPI
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- backend/app/routers/estudiantes.py
+++ backend/app/routers/estudiantes.py
@@ -1,18 +1,19 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database import get_db
-from app import models, schemas
+from app import models
+from app.schemas.estudiante import EstudianteOut, EstudianteCreate
 
 router = APIRouter(prefix="/estudiantes", tags=["Estudiantes"])
 
-@router.post("/", response_model=schemas.EstudianteOut)
-def crear_estudiante(estudiante: schemas.EstudianteCreate, db: Session = Depends(get_db)):
+@router.post("/", response_model=EstudianteOut)
+def crear_estudiante(estudiante: EstudianteCreate, db: Session = Depends(get_db)):
     nuevo = models.Estudiante(**estudiante.dict())
     db.add(nuevo)
     db.commit()
     db.refresh(nuevo)
     return nuevo
 
-@router.get("/", response_model=list[schemas.EstudianteOut])
+@router.get("/", response_model=list[EstudianteOut])
 def listar_estudiantes(db: Session = Depends(get_db)):
     return db.query(models.Estudiante).all()
